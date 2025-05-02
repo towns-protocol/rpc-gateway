6f113056bde87b4699a431fd4204c8713a7607ca
Kerem Kazan <kerem.kazan@gmail.com>
2025-05-02 18:54:54 -0400

    update bucketing settings 2

--- crates/rpc-gateway-core/src/metrics.rs
+++ crates/rpc-gateway-core/src/metrics.rs
@@ -22,7 +22,21 @@ pub fn run(config: &MetricsConfig) {
                 2.0,  // 2s
             ],
         )
-        .expect("failed to set buckets")
+        .expect("failed to set buckets for method_call_response_latency_seconds")
+        .set_buckets_for_metric(
+            Matcher::Full("http_response_latency_seconds".to_owned()),
+            &[
+                0.01, // 10ms
+                0.02, // 20ms
+                0.05, // 50ms
+                0.1,  // 100ms
+                0.2,  // 200ms
+                0.5,  // 500ms
+                1.0,  // 1s
+                2.0,  // 2s
+            ],
+        )
+        .expect("failed to set buckets for http_response_latency_seconds")
         .with_http_listener((host_bytes, config.port)) // listen on configured host:port
         .install() // returns Result
         .expect("failed to install Prometheus recorder");
