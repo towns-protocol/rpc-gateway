043879061f538d6290dcf42ffb9493f62fd4d411
Kerem Kazan <kerem.kazan@gmail.com>
2025-05-02 09:58:57 -0400

    remove reqres

--- crates/rpc-gateway-cache/src/redis.rs
+++ crates/rpc-gateway-cache/src/redis.rs
@@ -3,11 +3,9 @@ use std::{sync::Arc, time::Duration};
 use anvil_core::eth::EthRequest;
 use bb8::Pool;
 use bb8_redis::RedisConnectionManager;
-use redis::{AsyncCommands, FromRedisValue, RedisError, RedisWrite, ToRedisArgs};
+use redis::{AsyncCommands, RedisError};
 use rpc_gateway_config::RedisCacheConfig;
 use tracing::error;
-
-use crate::reqres::ReqRes;
 
 #[derive(Debug)]
 pub struct RedisCache {
@@ -54,7 +52,7 @@ impl RedisCache {
         format!("{}:{}", self.chain_id, serde_json::to_string(&req).unwrap()) // TODO: is this the right way to do this?
     }
 
-    pub async fn get(&self, req: &EthRequest) -> Option<ReqRes> {
+    pub async fn get(&self, req: &EthRequest) -> Option<serde_json::Value> {
         let key = self.get_key(req);
         let mut con = match self.pool.get().await {
             Ok(con) => con,
@@ -64,26 +62,30 @@ impl RedisCache {
             }
         };
 
-        let value: Result<Option<ReqRes>, _> = con.get(&key).await;
-        match value {
-            Ok(reqres) => reqres,
+        let value: Result<Option<String>, _> = con.get(&key).await;
+        let serde_value: Option<Result<serde_json::Value, serde_json::Error>> = match value {
+            Ok(value) => value.map(|v| serde_json::from_str(&v)),
             Err(e) => {
                 error!(
                     error = ?e,
                     req = ?req,
                     "Redis error",
                 );
+                return None;
+            }
+        };
+        match serde_value {
+            Some(Ok(value)) => Some(value),
+            Some(Err(e)) => {
+                error!(error = ?e, "Failed to deserialize Redis value");
                 None
             }
+            None => None,
         }
     }
 
     pub async fn insert(&self, req: &EthRequest, response: &serde_json::Value, ttl: Duration) {
         let key = self.get_key(req);
-        let reqres = ReqRes {
-            req: req.clone(),
-            res: response.clone(),
-        };
 
         // TODO: is there a better way to store the conneciton and reuse it?
         let mut connection = match self.pool.get().await {
@@ -97,7 +99,13 @@ impl RedisCache {
             }
         };
 
-        let result: Result<(), _> = connection.set_ex(&key, reqres, ttl.as_secs()).await;
+        let result: Result<(), _> = connection
+            .set_ex(
+                &key,
+                serde_json::to_string(response).unwrap(),
+                ttl.as_secs(),
+            )
+            .await;
         match result {
             Ok(_) => {}
             Err(err) => {
@@ -107,64 +115,5 @@ impl RedisCache {
                 );
             }
         }
-    }
-}
-
-impl FromRedisValue for ReqRes {
-    fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
-        match v {
-            redis::Value::SimpleString(s) => {
-                let reqres: ReqRes = serde_json::from_str(s).map_err(|e| {
-                    error!(
-                        error = ?e,
-                        value = ?s,
-                        "Failed to deserialize Redis value"
-                    );
-                    redis::RedisError::from((
-                        redis::ErrorKind::IoError,
-                        "Failed to deserialize Redis value",
-                        e.to_string(),
-                    ))
-                })?;
-                Ok(reqres)
-            }
-            redis::Value::BulkString(s) => {
-                let mut s = s.clone();
-                let reqres: ReqRes = serde_json::from_slice(&mut s).map_err(|e| {
-                    error!(
-                        error = ?e,
-                        value = ?s,
-                        "Failed to deserialize Redis value"
-                    );
-                    redis::RedisError::from((
-                        redis::ErrorKind::IoError,
-                        "Failed to deserialize Redis value",
-                        e.to_string(),
-                    ))
-                })?;
-                Ok(reqres)
-            }
-            _ => Err(redis::RedisError::from((
-                redis::ErrorKind::IoError,
-                "Expected a simple string. Received a: ",
-                format!("{:?}", v),
-            ))),
-        }
-    }
-}
-
-impl ToRedisArgs for ReqRes {
-    fn write_redis_args<W>(&self, out: &mut W)
-    where
-        W: ?Sized + RedisWrite,
-    {
-        // Serialize the ReqRes to a JSON string
-        let serialized = match serde_json::to_string(self) {
-            Ok(s) => s,
-            Err(_) => return, // Return early if serialization fails
-        };
-
-        // Write the serialized JSON string as a Redis argument
-        out.write_arg(serialized.as_bytes());
     }
 }
